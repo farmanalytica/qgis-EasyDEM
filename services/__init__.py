4c86531cc6804bcd7b0f598f03faf50b59eae1dd
liviascopel <livia.scopel@edu.ufes.br>
2026-04-15 09:07:52 -0300

    feat: DEMService using magma color palette

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,5 @@
 from .gee_service import GEEService
+from .aoi_service import AOIService
+from .dem_service import DEMService
 
-__all__ = ["GEEService"]
+__all__ = ["GEEService, AOIService, DEMService"]
